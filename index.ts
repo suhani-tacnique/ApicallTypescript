5de2f41385b99a5730db004dd18f29a1ca6bedf8
suhani-tacnique <124873318+suhani-tacnique@users.noreply.github.com>
2025-06-17 07:23:07 +0000

    trial

--- index.ts
+++ index.ts
@@ -8,7 +8,7 @@ interface Character {
     quidditchYears: number[];
   }
   
-  
+  // I tried
   fetch("https://coderbyte.com/api/challenges/json/quidditch-list")
     .then(result => result.json())
     .then(jsonformat => console.log(jsonformat));
